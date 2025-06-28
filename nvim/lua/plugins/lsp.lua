f5bb78fd8ab239d37f5d8dbcbeeb5a4f4e247e12
Sher <sher@Sher-Mac-mini.local>
2025-06-28 21:56:22 +0800

    [feat] add vscode settings and keybindings

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -5,11 +5,11 @@ return {
     opts = function()
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
       -- change a keymap
-      keys[#keys + 1] = { "K", "<cmd>echo 'hello'<cr>" }
+      -- keys[#keys + 1] = { "K", "<cmd>echo 'hello'<cr>" }
       -- disable a keymap
       keys[#keys + 1] = { "K", false }
       -- add a keymap
-      keys[#keys + 1] = { "H", "<cmd>echo 'hello'<cr>" }
+      -- keys[#keys + 1] = { "H", "<cmd>echo 'hello'<cr>" }
     end,
   },
 }
